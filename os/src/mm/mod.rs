889205593f6a9d7f6ae99b59f486aafc3492a9bd
KujouRinka <kujourinka@gmail.com>
2023-04-14 23:14:15 +0800

    ch4: feat: update mm/mod.rs

--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -1,7 +1,15 @@
 mod heap_allocator;
-
-pub use heap_allocator::heap_test;
+mod address;
+mod page_table;
+mod frame_allocator;
 
 pub fn init() {
   heap_allocator::init_heap();
+  frame_allocator::init_frame_allocator();
+}
+
+#[allow(unused)]
+pub fn test() {
+  heap_allocator::heap_test();
+  frame_allocator::frame_allocator_test();
 }
