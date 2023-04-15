32895af13d506a6abbc9bda078d50af82839f8d1
KujouRinka <kujourinka@gmail.com>
2023-04-15 17:39:06 +0800

    ch4: feat: impl memory set

--- os/src/config.rs
+++ os/src/config.rs
@@ -3,9 +3,16 @@
 pub const USER_STACK_SIZE: usize = 1 << 13;
 pub const KERNEL_STACK_SIZE: usize = 1 << 13;
 pub const MAX_APP_NUM: usize = 16;
-pub const APP_BASE_ADDRESS: usize = 0x80400000;
-pub const APP_SIZE_LIMIT: usize = 0x20000;
 pub const CLOCK_FREQ: usize = 12500000;
 
 // mm
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+pub const PAGE_SIZE_BITS: usize = 0xc;
+pub const PAGE_SIZE: usize = 1 << PAGE_SIZE_BITS;   // 4k
+pub const PTE_FLAGS_BITS: usize = 0xa;
+
+pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
+pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
+
+// pub const MEMORY_END: usize = 0x88000000;  // 128M
+pub const MEMORY_END: usize = 0x80800000;     // 8M
--- os/src/loader.rs
+++ os/src/loader.rs
@@ -1,52 +1,4 @@
-use core::arch::asm;
-use crate::trap::context::TrapContext;
 use crate::config::*;
-
-#[repr(align(4096))]
-#[derive(Copy, Clone)]
-struct KernelStack {
-  data: [u8; KERNEL_STACK_SIZE],
-}
-
-#[repr(align(4096))]
-#[derive(Copy, Clone)]
-struct UserStack {
-  data: [u8; USER_STACK_SIZE],
-}
-
-static KERNEL_STACK: [KernelStack; MAX_APP_NUM] = [
-  KernelStack { data: [0; KERNEL_STACK_SIZE] };
-  MAX_APP_NUM
-];
-
-static USER_STACK: [UserStack; MAX_APP_NUM] = [
-  UserStack { data: [0; USER_STACK_SIZE] };
-  MAX_APP_NUM
-];
-
-impl KernelStack {
-  fn get_sp(&self) -> usize {
-    self.data.as_ptr() as usize + KERNEL_STACK_SIZE
-  }
-
-  pub fn push_context(&self, cx: TrapContext) -> usize {
-    let cx_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
-    unsafe {
-      *cx_ptr = cx;
-    }
-    cx_ptr as usize
-  }
-}
-
-impl UserStack {
-  fn get_sp(&self) -> usize {
-    self.data.as_ptr() as usize + USER_STACK_SIZE
-  }
-}
-
-fn get_base_i(app_id: usize) -> usize {
-  APP_BASE_ADDRESS + APP_SIZE_LIMIT * app_id
-}
 
 pub fn get_num_app() -> usize {
   extern "C" {
@@ -55,40 +7,19 @@ pub fn get_num_app() -> usize {
   unsafe { (_num_app as *const usize).read_volatile() }
 }
 
-pub fn load_apps() {
+pub fn get_app_data(app_id: usize) -> &'static [u8] {
   extern "C" { fn _num_app(); }
   let num_app_ptr = _num_app as *const usize;
   let num_app = get_num_app();
+  assert!(app_id < num_app);
   let app_start = unsafe {
     core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1)
   };
-  for i in 0..num_app {
-    let base_i = get_base_i(i);
-    unsafe {
-      core::ptr::write_bytes(
-        base_i as *mut u8,
-        0,
-        APP_SIZE_LIMIT,
-      );
-    }
-    // Copy app bytes from src to dst
-    unsafe {
-      core::ptr::copy(
-        app_start[i] as *const u8,
-        base_i as *mut u8,
-        app_start[i + 1] - app_start[i],
-      );
-    }
-  }
+  // Copy app bytes from src to dst
   unsafe {
-    asm!("fence.i");
+    core::slice::from_raw_parts(
+      app_start[app_id] as *const u8,
+      app_start[app_id + 1] - app_start[app_id],
+    )
   }
-}
-
-/// Set app entry and user stack for first run.
-pub fn init_app_cx(app_id: usize) -> usize {
-  KERNEL_STACK[app_id].push_context(TrapContext::app_init_context(
-    get_base_i(app_id),
-    USER_STACK[app_id].get_sp(),
-  ))
 }
